f9258a00b4a23fa9444d8b064fbf4bec48034b5e
joaosantosmedeiros <jopesame@gmail.com>
2023-12-22 09:58:02 -0300

    fix: add constraint NotNull on ProductRecordDto value field

--- src/main/java/com/example/produtosapi/dtos/ProductRecordDto.java
+++ src/main/java/com/example/produtosapi/dtos/ProductRecordDto.java
@@ -3,8 +3,9 @@ package com.example.produtosapi.dtos;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record ProductRecordDto(@NotBlank String name, @Positive BigDecimal value) {
+public record ProductRecordDto(@NotBlank String name, @Positive @NotNull BigDecimal value) {
   
 }
